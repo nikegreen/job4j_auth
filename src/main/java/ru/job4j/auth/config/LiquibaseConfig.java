a899c8261b8f70b41465140b394ac36e340d6709
nikez <nikegreen@yandex.ru>
2023-03-04 19:22:58 +0500

    1. RESTFul. Описание архитектуры [#6884 #389428]

---
+++ src/main/java/ru/job4j/auth/config/LiquibaseConfig.java
@@ -0,0 +1,17 @@
+package ru.job4j.auth.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+}
--- src/test/java/ru/job4j/auth/AuthApplicationTests.java
+++ src/test/java/ru/job4j/auth/AuthApplicationTests.java
@@ -2,12 +2,14 @@ package ru.job4j.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isEqualTo(true);
 	}
 
 }
